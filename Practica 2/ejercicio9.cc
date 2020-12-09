6affd425a3098a03ee5519c4bb0ed6637f41c644
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-12-09 19:08:40 +0100

    Create ejercicio9.cc

---
+++ Practica 2/ejercicio9.cc
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+unsigned int maj, min;
+
+if(argc != 2)    
+	return 1;
+
+struct stat fileStat;
+
+if(stat(argv[1],&fileStat) < 0)    
+	return 1;
+
+maj = major(fileStat.st_dev);
+printf("Major: %i\n", maj);
+
+min = minor(fileStat.st_dev);
+printf("Minor: %i\n", min);
+
+printf("Inodo: %d\n",fileStat.st_ino);
+
+printf("Tipo fichero: %d\n",fileStat.st_mode);
+
+if((fileStat.st_mode & S_IFMT) == S_IFREG){
+	printf("Es un fichero ordinario.\n");
+}else if((fileStat.st_mode & S_IFMT) == S_IFDIR){
+	printf("Es un directorio.\n");
+}else if((fileStat.st_mode & S_IFMT) == S_IFLNK){
+	printf("Es un enlace simbolico.\n");
+}
+
+char timer[80]; 
+strftime(timer, 80, "%A, %d de %B de %Y, %R", localtime(&fileStat.st_atime));
+printf("Ultima vez que se accedio al fichero: %s\n",timer);
+
+/*
+st_mtime: ultima modificacion de datos del archivo
+st_ctime: ultimo cambio de estado del archivo
+*/
+
+return 0;		
+}
