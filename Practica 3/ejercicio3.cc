3725e992ae05a852080c78d8b269b26a4c9a317b
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-12-14 18:20:45 +0100

    Create ejercicio3.cc

---
+++ Practica 3/ejercicio3.cc
@@ -0,0 +1,9 @@
+/*
+sh-4.2# sudo ./ejercicio2 1588
+Politica de planificación: SCHEDULE FIFO
+La prioridad maxima es: 99
+La prioridad minima es: 1
+La prioridad asiganda es: 12
+
+Se heredan los artibutos de planificación
+*/
