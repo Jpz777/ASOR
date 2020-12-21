6092912c523460aec12ac6de92f0f57cf2d0d382
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-12-21 20:44:03 +0100

    ejercicio 11 era ejercicio 10, se ha añadido ejercicio 11 ahora

---
+++ Practica 3/ejercicio10.cc
@@ -0,0 +1,6 @@
+/*
+En una shell se escribe sleep(600)
+
+En la otra shell se escribe sudo kill -SIGKILL "proceso sleep" y se elimina el proceso (la shell no está ejecutando el sleep).
+
+*/
--- Practica 3/ejercicio11.cc
+++ Practica 3/ejercicio11.cc
@@ -1,6 +1,44 @@
-/*
-En una shell se escribe sleep(600)
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
 
-En la otra shell se escribe sudo kill -SIGKILL "proceso sleep" y se elimina el proceso (la shell no está ejecutando el sleep).
+#define SLEEP_SECS 5
 
-*/
+int main() {
+sigset_t blk_set;
+
+sigemptyset(&blk_set);
+sigaddset(&blk_set, SIGINT);
+sigaddset(&blk_set, SIGTSTP);
+
+sigprocmask(SIG_BLOCK, &blk_set, NULL);
+
+printf("El proceso se duerme %i segundos\n", SLEEP_SECS);
+sleep(SLEEP_SECS);
+printf("El proceso se ha despertado\n", SLEEP_SECS);
+
+sigset_t sigset;
+
+if(sigpending(&sigset) != 0 )
+    perror( "sigpending() error\n" );
+
+if(sigismember(&sigset, SIGINT)){
+    printf("La señal SIGINT sigue pendiante\n");
+	sigdelset(&blk_set, SIGTSTP);
+}
+else {
+    printf("La señal SIGINT no sigue pendiante\n");
+}
+if(sigismember(&sigset, SIGTSTP)) {
+    printf("La señal SIGTSTP sigue pendiante\n");
+    sigdelset(&blk_set, SIGINT);
+}
+else {
+    printf("La señal SIGSTP no sigue pendiante\n");
+}
+
+sigprocmask(SIG_UNBLOCK, &blk_set, NULL);
+
+return 0;
+}
