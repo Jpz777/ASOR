5406baed953c01b81b47197ceb879eb50ea1b2d8
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-12-21 18:37:38 +0100

    ejercicio 7

---
+++ Practica 3/ejercicio7execvp.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>      
+#include <stdlib.h> 
+
+int main(int argc, char **argv) {
+if(argc != 2) {
+	printf("Se necesitan dos parametros\n");
+	return 1;
+}
+
+if (execvp(argv[1], argv + 1) == -1)
+	printf("No se ha ejecutado el comando\n");
+
+printf("El comando terminó de ejecutarse\n");
+
+return 0;
+}
+//La manera de ejecutar es ./ficheroejecutable "ls -l"
+//No se ejecuta el printf dado que el execvp reemplaza la imagen del proceso actual con una nueva imagen del proceso
---
+++ Practica 3/ejercicio7system.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>      
+#include <stdlib.h> 
+#include <string.h>    
+
+int main(int argc, char **argv) {
+if(argc != 2) {
+	printf("Se necesitan dos parametros\n");
+	return 1;
+}
+
+int i;
+int lengthargv;
+
+if (system(argv[1]) == -1) 
+	printf("No se ha ejecutado el comando\n");
+
+printf("El comando terminó de ejecutarse\n");
+
+return 0;
+}
+
+//La manera de ejecutar es ./ficheroejecutable "ls -l"
+//Se ejecuta el printf
