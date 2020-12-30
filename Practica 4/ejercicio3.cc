27dc576b3403bea770d5ee063ccaa8930ea3c719
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-12-30 18:11:50 +0100

    Create ejercicio3.cc

---
+++ Practica 4/ejercicio3.cc
@@ -0,0 +1,14 @@
+/*
+se crea un nombre para el pipe con el comando mkfifo
+
+"mkfifo nombrepipe"
+
+si quieres ver un comando en otra terminal lo que debes hacer es, en la primera terminal
+
+"comando > nombrepipe"
+
+y en la segunda terminal
+
+"cat < nombrepipe"
+
+*/
